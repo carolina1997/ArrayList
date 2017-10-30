ab2a4d7eb4c094717e8c89c278bc3e1cea1c2cdf
Carolina Priscila <caropriscila01@gmail.com>
2017-10-30 10:23:36 -0300

    Add files via upload

---
+++ Practica_Pair_v1.2/Windows_64/Employee.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+    return 0;
+}
+
+
+void employee_print(Employee* this)
+{
+
+    printf("%d--%s--%s--%d\n", this->id, this->name, this->lastName, this->isEmpty);
+
+}
+
+
+Employee* employee_new(void)
+{
+
+   Employee* returnAux = (Employee*) malloc(sizeof(Employee));
+
+    return returnAux;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+
+    return 0;
+
+}
+
+int employee_getId(Employee* this)
+{
+
+    return 0;
+
+}
+
+
---
+++ Practica_Pair_v1.2/Windows_64/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+   ArrayList* lista = al_newArrayList();
+   Employee* aux;
+   int i;
+
+   Employee* e1= employee_new();
+   Employee* e2= employee_new();
+
+   e1->id= 2;
+   strcpy(e1->name, "aldana");
+   strcpy(e1->lastName, "salvatierra");
+   e1->isEmpty=1;
+
+   e2->id= 5;
+   strcpy(e2->name, "carolina");
+   strcpy(e2->lastName, "ojeda");
+   e2->isEmpty=1;
+
+   al_add(lista, e1);
+   al_add(lista,e2);
+
+   printf("la cantidad de elementos son: %d\n\n", al_len(lista));
+
+   for(i=0;i<al_len(lista);i++)
+   {
+       aux=al_get(lista, i);
+       employee_print(aux);
+   }
+
+    return 0;
+}
